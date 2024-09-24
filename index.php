8a5bb78b61a9f8186f6637cc78cbce902b928b54
Yoga_Axel_Baim_Pratama <23_yogaxelbaim@student.smkti.net>
2024-09-24 22:05:10 +0800

    Menambahkan tampilan ucapan dan doa

--- index.php
+++ index.php
@@ -36,7 +36,10 @@
         </div>
 
         <br>
-
+        <div style = "background:black;color:white">
+            <h3>Lokasi Acara</h3>
+            <h3></h3>
+        </div>
         <iframe width="425" height="350"
             src="https://www.openstreetmap.org/export/embed.html?bbox=2.1725463867187504%2C48.7935210706521%2C2.435531616210938%2C48.92407837067149&amp;layer=mapnik"
             style="border: 1px solid rgb(0, 0, 0)"></iframe><br /><small><a
@@ -44,10 +47,44 @@
         <h4>Lihat peta lebih besar</h4>
         </a></small>
         </a>
+    <form action="insert.php" method="post">
+        <div style = "background:black;color:white">
+        <strong><h3>Ucapan & Doa</h3>
+        </strong>
+        </div>
+        <form action="" method="post">
+            <input type="text" name="nama" placeholder="Nama"> <br>
+            <textarea name="ucapan" cols="30" rows="4" placeholder="Ucapan"> </textarea> <br>
+            <select name="Keterangan">
+                <option value="" selected disabled hidden> Konfirmasi Kehadiran</option>
+                <option value="1">Ya</option>
+                <option value="2">Tidak Hadir</option>
+                <option value="3">Tidak Tau</option>
+            </select> <br>
+            <button type="submit">Kirim</button>
+        </form>
+        <?php
+        include 'koneksi.php';
+        $SQL2 = "SELECT * FROM bukutamu ORDER BY id DESC";
+        $hasil = $connection->query($SQL2);
+        ?>
+        <div style="border: 1px solid black;margin:auto; text-align:center; height:200px; width:300px; overflow:scroll;">
+            <?php
 
+            while ($baris = $hasil->fetch_row()) {
+            ?>
+                <div style="background:white;border: 1px solid black;border-color:black; margin:5px; padding:5px;">
+                    <p style="font-weight:bold;"><?= $baris[1] ?></p>
+                    <p><?= $baris[2] ?></p>
+                </div>
+
+            <?php
+            }
+
+            $hasil->free_result();
+            ?>
         <audio controls autoplay loop src="music.mp3"></audio>
-    </div>
-
+        </div>
 </body>
 
 </html>
